7d4a0c552b83afd34c0027e012370187f1a6bdec
IJumpAround <>
2020-11-22 19:12:13 -0500

    Make IDE stop complaining.

--- role.upgrader.js
+++ role.upgrader.js
@@ -15,27 +15,27 @@ var roleUpgrader = {
 
         //Start upgrading controller
 	    if(creep.memory.upgrading) {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+            if(creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
             }
         }
         //pick up resources
         else {
-            var containers = creep.room.find(FIND_STRUCTURES,{
-                filter : (i) => i.structureType == STRUCTURE_STORAGE &&
+            let containers = creep.room.find(FIND_STRUCTURES,{
+                filter : (i) => i.structureType === STRUCTURE_STORAGE &&
                                 i.store[RESOURCE_ENERGY] > i.storeCapacity * 0.02
                 });
             //Withdraw from containers first
             if(containers.length > 0){
-                closest = creep.pos.findClosestByPath(containers);
-                if(creep.withdraw(closest,RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+                let closest = creep.pos.findClosestByPath(containers);
+                if(creep.withdraw(closest,RESOURCE_ENERGY) === ERR_NOT_IN_RANGE)
                     creep.moveTo(closest,{visualizePathStyle: {stroke: '#ffaa00'}});
 
             }
            
             else{
-                var droppedEnergy = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
-                if(creep.pickup(droppedEnergy) == ERR_NOT_IN_RANGE) {
+                let droppedEnergy = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES);
+                if(creep.pickup(droppedEnergy) === ERR_NOT_IN_RANGE) {
                     creep.moveTo(droppedEnergy, {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
             }
